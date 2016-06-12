ca7f5ae24d380c1145a592f9672966dbed34bc56
Maximilian Weber <weber.maximilian@googlemail.com>
2016-06-12 18:16:34 +0200

    Extracted sv.system.datomic from sv/system.

---
+++ README.md
@@ -0,0 +1,11 @@
+# system.datomic
+
+Components and helper functions to use
+[Datomic](http://www.datomic.com/) in a sv/system-based application.
+
+## License
+
+Copyright © 2016 Max Weber (SimpleValue)
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject sv/system.datomic "0.1.0-SNAPSHOT"
+  :description "Components and helper functions to use Datomic in a
+                sv/system-based application."
+  :url "https://github.com/SimpleValue/sv.system.datomic"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.2.374"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [com.datomic/datomic-free "0.9.5372"]])
